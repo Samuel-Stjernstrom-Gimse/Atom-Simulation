5524c00ae621ace560ee86cb7cc9a3878f7f7abe
Zam <samuel.kodehode@gmail.com>
2024-02-10 12:34:33 +0100

    1.2.0

--- script.js
+++ script.js
@@ -2,11 +2,65 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const particles = [];
+const p1Count = document.getElementById('input-number1');
+const p2Count = document.getElementById('input-number2');
+const p3Count = document.getElementById('input-number3');
+const p1Color = document.getElementById('input-color1');
+const p2Color = document.getElementById('input-color2');
+const p3Color = document.getElementById('input-color3');
+const p1p1Button = document.getElementById('particle1-particle1');
+const p2p2Button = document.getElementById('particle1-particle1');
+const p3p3Button = document.getElementById('particle1-particle1');
+let p1p1 = true;
+let p2p2 = true;
+let p3p3 = true;
+const p1ForceInput = document.getElementById('particle1-particle1-force');
+const p2ForceInput = document.getElementById('particle2-particle2-force');
+const p3ForceInput = document.getElementById('particle3-particle3-force');
+let p2p2a = 10;
+let p3p3a = 10;
+let p1Range = document.getElementById('particle1-particle1-range');
+const p2Range = document.getElementById('particle2-particle2-range');
+const p3Range = document.getElementById('particle2-particle2-range');
+p1p1Button.addEventListener('click', () => {
+    if (!p1p1) {
+        p1p1 = true;
+    }
+    else if (p1p1) {
+        p1p1 = false;
+    }
+});
+p2p2Button.addEventListener('click', () => {
+    if (p2p2 === false) {
+        p2p2 = true;
+        p2p2a = p2ForceInput.valueAsNumber;
+    }
+    else if (p2p2 === true) {
+        p2p2a = -p2ForceInput.valueAsNumber;
+        p2p2 = false;
+    }
+});
+p3p3Button.addEventListener('click', () => {
+    if (p3p3 === false) {
+        p3p3 = true;
+        p3p3a = p3ForceInput.valueAsNumber;
+    }
+    else if (p3p3 === true) {
+        p3p3a = -p3ForceInput.valueAsNumber;
+        p3p3 = false;
+    }
+});
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 function draw(x, y, c, s) {
     ctx.fillStyle = c;
     ctx.fillRect(x, y, s, s);
+}
+function drawParticle(x, y, c, s) {
+    ctx.beginPath();
+    ctx.arc(x, y, s / 2, 0, 2 * Math.PI);
+    ctx.fillStyle = c;
+    ctx.fill();
 }
 const particle = (x, y, c) => {
     return { x: x, y: y, vx: 0, vy: 0, color: c };
@@ -38,8 +92,8 @@ const rule = (particles1, particles2, g, range) => {
                 fy += (F * dy) / d;
             }
         }
-        a.vx = (a.vx + fx) * 0.9;
-        a.vy = (a.vy + fy) * 0.9;
+        a.vx = (a.vx + fx) * 0.8;
+        a.vy = (a.vy + fy) * 0.8;
         a.x += a.vx;
         a.y += a.vy;
         if (a.x <= 0 || a.x >= canvas.width) {
@@ -50,25 +104,48 @@ const rule = (particles1, particles2, g, range) => {
         }
     }
 };
-let yellow = create(2500, 'yellow');
-let red = create(1000, 'red');
-let white = create(1000, 'white');
-let blue = create(1000, 'rgb(0,215,255)');
+let red = create();
+let green = create();
+let cyan = create();
+p1Count.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        red = create(p1Count.valueAsNumber, p1Color.value);
+    }
+});
+p2Count.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        let green = create(p2Count.valueAsNumber, p2Color.value);
+    }
+});
+p3Count.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        cyan = create(p3Count.valueAsNumber, p3Color.value);
+    }
+});
 const update = () => {
-    rule(red, red, 0.8, 100);
-    rule(red, white, 0.1, 100);
-    rule(yellow, red, 2.9, 120);
-    rule(yellow, red, -5.5, 60);
-    rule(yellow, yellow, -0.3, 12);
-    rule(white, white, -0.9, 20);
-    rule(white, red, 0.9, 300);
-    rule(blue, red, 2.9, 90);
-    rule(blue, red, -3.5, 34);
-    rule(blue, blue, -3.5, 10);
+    let heyo = p1Range.valueAsNumber ? p1Range.valueAsNumber : 1;
+    let p1p1a = p1p1 ? p1ForceInput.valueAsNumber : -p1ForceInput.valueAsNumber;
+    if (isNaN(p1p1a)) {
+        p1p1a = 1;
+    }
+    rule(red, red, -2, 12);
+    rule(red, red, p1p1a ? p1p1a : 0.0001, heyo);
+    console.log(p1Range.valueAsNumber);
+    console.log(heyo);
+    rule(cyan, cyan, -1, 12);
+    rule(cyan, cyan, p2p2a ? p2p2a : 1, 200);
+    rule(green, green, -1, 12);
+    rule(green, green, p3p3a, p3Range.valueAsNumber);
+    rule(red, cyan, -0.22, 40);
+    rule(red, green, -0.22, 40);
+    rule(cyan, red, -0.22, 40);
+    rule(cyan, green, -0.22, 40);
+    rule(green, red, -0.22, 40);
+    rule(green, cyan, -0.22, 40);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw(0, 0, 'rgb(0,0,0)', canvas.width);
     for (let i = 0; i < particles.length; i++) {
-        draw(particles[i].x, particles[i].y, particles[i].color, 2);
+        drawParticle(particles[i].x, particles[i].y, particles[i].color, 10);
     }
 };
 setInterval(update, 36);
--- script.ts
+++ script.ts
@@ -2,6 +2,64 @@ const canvas = document.getElementById('canvas') as HTMLCanvasElement
 const ctx = canvas.getContext('2d')
 const particles: any[] = []
 
+const p1Count:HTMLInputElement = document.getElementById('input-number1') as HTMLInputElement
+const p2Count:HTMLInputElement = document.getElementById('input-number2') as HTMLInputElement
+const p3Count:HTMLInputElement = document.getElementById('input-number3') as HTMLInputElement
+
+const p1Color:HTMLInputElement = document.getElementById('input-color1') as HTMLInputElement
+const p2Color:HTMLInputElement = document.getElementById('input-color2') as HTMLInputElement
+const p3Color:HTMLInputElement = document.getElementById('input-color3') as HTMLInputElement
+
+const p1p1Button = document.getElementById('particle1-particle1') as HTMLButtonElement
+const p2p2Button = document.getElementById('particle1-particle1') as HTMLButtonElement
+const p3p3Button = document.getElementById('particle1-particle1') as HTMLButtonElement
+
+let p1p1 = true
+let p2p2 = true
+let p3p3 = true
+
+const p1ForceInput = document.getElementById('particle1-particle1-force') as HTMLInputElement
+const p2ForceInput = document.getElementById('particle2-particle2-force') as HTMLInputElement
+const p3ForceInput = document.getElementById('particle3-particle3-force') as HTMLInputElement
+
+
+let p2p2a = 10
+let p3p3a = 10
+
+let p1Range = document.getElementById('particle1-particle1-range') as HTMLInputElement
+const p2Range = document.getElementById('particle2-particle2-range') as HTMLInputElement
+const p3Range = document.getElementById('particle2-particle2-range') as HTMLInputElement
+
+
+p1p1Button.addEventListener('click', () => {
+	if (!p1p1) {
+		p1p1 = true
+	} else if (p1p1) {
+		p1p1 = false
+	}
+})
+
+p2p2Button.addEventListener('click', () => {
+	if (p2p2 === false) {
+		p2p2 = true
+		p2p2a = p2ForceInput.valueAsNumber
+	} else if (p2p2 === true) {
+		p2p2a = -p2ForceInput.valueAsNumber
+		p2p2 = false
+	}
+})
+
+p3p3Button.addEventListener('click', () => {
+	if (p3p3 === false) {
+		p3p3 = true
+		p3p3a = p3ForceInput.valueAsNumber
+	} else if (p3p3 === true) {
+		p3p3a = -p3ForceInput.valueAsNumber
+		p3p3 = false
+	}
+})
+
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -11,6 +69,15 @@ function draw(x: number, y: number, c: string, s: number): void {
 	// @ts-ignore
 	ctx.fillRect(x, y, s, s)
 }
+
+function drawParticle(x, y, c, s) {
+	ctx.beginPath()
+	ctx.arc(x,y,s / 2, 0, 2*Math.PI)
+	ctx.fillStyle = c
+	ctx.fill()
+}
+
+
 
 const particle = (x: number, y: number, c: string) => {
 	return { x: x, y: y, vx: 0, vy: 0, color: c }
@@ -48,8 +115,8 @@ const rule = (particles1: any, particles2: any, g: number, range: number) => {
 				fy += (F * dy) / d
 			}
 		}
-		a.vx = (a.vx + fx) * 0.9
-		a.vy = (a.vy + fy) * 0.9
+		a.vx = (a.vx + fx) * 0.8
+		a.vy = (a.vy + fy) * 0.8
 		a.x += a.vx
 		a.y += a.vy
 		if (a.x <= 0 || a.x >= canvas.width) {
@@ -61,36 +128,82 @@ const rule = (particles1: any, particles2: any, g: number, range: number) => {
 	}
 }
 
-let yellow: {x: number, y: number, vx: number, vy: number, color: string}[] = create(2500, 'yellow')
-let red: {x: number, y: number, vx: number, vy: number, color: string}[] = create(1000, 'red')
-let white: {x: number, y: number, vx: number, vy: number, color: string}[] = create(1000, 'white')
-let blue: {x: number, y: number, vx: number, vy: number, color: string}[] = create(1000, 'rgb(0,215,255)')
+
+let red = create()
+let green = create()
+let cyan = create()
+p1Count.addEventListener('keypress', (e) => {
+
+	if (e.key === 'Enter') {
+		red = create(p1Count.valueAsNumber, p1Color.value)
+	}
+})
+
+p2Count.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter'){
+		let green = create(p2Count.valueAsNumber, p2Color.value)
+	}
+})
+
+p3Count.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter'){
+		cyan = create(p3Count.valueAsNumber, p3Color.value)
+	}
+})
+
+
+
 
 const update = () => {
-	rule(red, red, 0.8, 100)
-	rule(red, white, 0.1, 100)
-	rule(yellow, red, 2.9, 120)
-	rule(yellow, red, -5.5, 60)
-	rule(yellow, yellow, -0.3, 12)
-	rule(white, white, -0.9, 20)
-	/*	rule(blue, red, -0.2, 2)*/
-	rule(white, red, 0.9, 300)
-	rule(blue, red, 2.9, 90)
-	rule(blue, red, -3.5, 34)
-	rule(blue, blue, -3.5, 10)
-/*	rule(blue, red, -10, 80)
-	rule(blue, blue, -0.01, 10)*/
-	/*	rule(blue, red, -1.4, 10)*/
-	/*        rule(red, red, 1)
-			rule(red, yellow, -0.1)
-			rule(yellow, red, -0.1)*/
+	let heyo = p1Range.valueAsNumber ? p1Range.valueAsNumber : 1
+	let p1p1a = p1p1 ? p1ForceInput.valueAsNumber : -p1ForceInput.valueAsNumber
+
+	if (isNaN(p1p1a)) {
+		p1p1a = 1
+	}
+
+
+	rule(red, red, -2, 12)
+	rule(red, red, p1p1a ? p1p1a : 0.0001, heyo)
+
+	console.log(p1Range.valueAsNumber)
+	console.log(heyo)
+
+	rule(cyan, cyan, -1, 12)
+	rule(cyan, cyan, p2p2a ? p2p2a : 1, 200)
+
+	rule(green, green, -1, 12)
+	rule(green, green, p3p3a, p3Range.valueAsNumber)
+
+	rule(red, cyan, -0.22, 40)
+	rule(red, green, -0.22, 40)
+
+	rule(cyan, red, -0.22, 40)
+	rule(cyan, green, -0.22, 40)
+
+	rule(green, red, -0.22, 40)
+	rule(green, cyan, -0.22, 40)
+
+
+	//own design
+	/*	rule(red, red, 0.8, 100)
+        rule(red, white, 0.1, 100)
+        rule(yellow, red, 2.9, 120)
+        rule(yellow, red, -5.5, 60)
+        rule(yellow, yellow, -0.3, 12)
+        rule(white, white, -0.9, 20)
+        rule(white, red, 0.9, 300)
+        rule(blue, red, 2.9, 90)
+        rule(blue, red, -3.5, 34)
+        rule(blue, blue, -3.5, 10)*/
+
 	// @ts-ignore
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-	draw(0, 0, 'rgb(0,0,0)', canvas.width)
+	draw(0, 0, 'rgb(0,0,0)', canvas.width) //rect
 
 	for (let i = 0; i < particles.length; i++) {
-		draw(particles[i].x, particles[i].y, particles[i].color, 2)
+		drawParticle(particles[i].x, particles[i].y, particles[i].color, 10)
 	}
 
 	/*requestAnimationFrame(update)*/
